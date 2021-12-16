959dd46b379d34d07fdcfe5d230b3ec607a5f2c8
Gaurav <gauravKatwate7891@gmail.com>
2021-12-16 20:54:20 +0530

    Python Programs

---
+++ Basic Programs/01.Addition Of Two Numbers.py
@@ -0,0 +1,12 @@
+"""
+@gole: Addition Of Two Numbers
+@date:15-12-2021
+
+"""
+
+ino1 = int(input("Enter First Number =>"))
+ino2 = int(input("Enter Second Number =>"))
+
+Add = ino1 + ino2
+
+print("Addition Of Two Numbers Is => ",Add)

87b00b52e16a8c5f61c0d6e8cf44b32bfa81513a
Gaurav <gauravKatwate7891@gmail.com>
2022-01-02 22:48:28 +0530

    Python Program

---
+++ Conditional Statement And Loops/06.Inputed Number Divisible Both 5 & 7 or  Only 5 or Only 7 or Not Both 5 & 7.py
@@ -0,0 +1,18 @@
+"""
+@gole : inputted no. is divisible by both 5 & 7 or only by 5 or only by 7 or not by both 5 & 7.
+@date : 1-1-2022
+"""
+
+ino = int(input("Enter Any Numbe =>"))
+
+if ino % 5 == 0 and ino % 7 == 0:
+    print("Given Number Divisible Both 5 & 7.")
+elif ino % 5 == 0:
+    print("Given Number Divisible By 5.")
+elif ino % 7 == 0:
+    print("Given Number Divisible By 7.")
+else:
+    print("Given Number Not Divisible Both 5 & 7")
+
+print("Thanks...")
+input()

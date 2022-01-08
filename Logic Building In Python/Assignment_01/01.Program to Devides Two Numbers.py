2cb7cb4c54413af45db36c44beed214f2a0b02fe
Gaurav <gauravKatwate7891@gmail.com>
2022-01-09 00:58:39 +0530

    Logic Building in Python

---
+++ Logic Building In Python/Assignment_01/01.Program to Devides Two Numbers.py
@@ -0,0 +1,15 @@
+"""
+@gole : Accept Two Numbers And Retuen Division
+@date : 9-1-2022
+"""
+
+ino1 = int(input("Enter First Number =>"))
+ino2 = int(input("Enter Second Number =>"))
+
+def Div(ino1,ino2):
+    return ino1 // ino2
+
+print("Division Of Two Numbers =>",Div(ino1,ino2))
+
+input()
+print("Thanks")

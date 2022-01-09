3d880cb040e84ba1f893c01c309aa522c9abcf60
Gaurav <gauravKatwate7891@gmail.com>
2022-01-10 03:08:41 +0530

    Python Programs

---
+++ Logic Building In Python/Assignment_01/05.Accept Number And Print Number Of # On Screen.py
@@ -0,0 +1,12 @@
+"""
+@gole : Accept Number And Print Number Of * in Screen
+@date : 10-1-2022
+"""
+
+def Print(no):
+    for i in range(no):
+        print(" * ")
+
+Print(int(input("Enter Any Number =>")))
+
+input()

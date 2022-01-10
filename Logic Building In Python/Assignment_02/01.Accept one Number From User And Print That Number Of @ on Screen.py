de4067017f198c09760550e2f02465b617f182a1
Gaurav <gauravKatwate7891@gmail.com>
2022-01-10 19:28:55 +0530

    Python Programs

---
+++ Logic Building In Python/Assignment_02/01.Accept one Number From User And Print That Number Of @ on Screen.py
@@ -0,0 +1,14 @@
+"""
+@gole : Print That Number Of @ On Screen
+@date : 10-1-2022
+"""
+
+def Display(ino):
+    while 0 < ino:
+        print(" @ ")
+        ino = ino - 1
+
+Display(int(input("Enter Any Number =>")))
+
+input()
+print("Thanks")

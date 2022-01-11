231797963ac1f853dccb994c44643d908bb963b6
Gaurav <gauravKatwate7891@gmail.com>
2022-01-11 20:30:35 +0530

    Python Programs

---
+++ Logic Building In Python/Assignment_02/03.Accept Any Number From User And Display.py
@@ -0,0 +1,16 @@
+"""
+@gole : Accept Any Number From if Number Is Less Then 10 Then  Print "Hello"
+        Otherwise Print "Demo"
+@date : 11-1-2022
+"""
+
+def Display(no):
+    if no < 10:
+        print("Hello")
+    else:
+        print("Demo")
+
+Display(int(input("Enter Any Number =>")))
+
+input()
+print("Thanks")

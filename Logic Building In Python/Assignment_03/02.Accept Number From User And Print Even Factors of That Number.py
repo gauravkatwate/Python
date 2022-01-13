38388b38d01c96a5a8c4a1cb65f81a6390dd01d3
Gaurav <gauravKatwate7891@gmail.com>
2022-01-13 22:15:30 +0530

    Python Programs

---
+++ Logic Building In Python/Assignment_03/02.Accept Number From User And Print Even Factors of That Number.py
@@ -0,0 +1,18 @@
+"""
+@gole : Accept Number From User And Print Even Factors Of That Number
+@date : 13-1-2022
+"""
+
+def Display(no):
+    if no <= 0:
+        return 0
+    temp = no // 2
+    
+    for i in range(1,temp + 1):
+        if no % i == 0:
+            print(i)
+        
+
+Display(int(input("Enter Any Number =>")))
+
+input()
